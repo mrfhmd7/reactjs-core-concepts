ca9bfd9f3dd5ae44dae72fabe19c7c2c6b14b615
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-23 22:54:33 +0600

    nayok, nayika and singer added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 1234;
-const singer = { name: 'Dr. Mahfuz', job: 'Singer' };
-const singer2 = { name: 'Eva Rahman', job: 'Singer' };
+// const singer = { name: 'Dr. Mahfuz', job: 'Singer' };
+// const singer2 = { name: 'Eva Rahman', job: 'Singer' };
+
+const singers = [
+  { name: 'Dr. Mahfuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer' },
+  { name: 'Agun', job: 'Amar Shopno' },
+  { name: 'Kumar Sanu', job: 'Aguner Din' }
+]
+
 const singerStyle = {
   color: 'purple',
   backgroundColor: 'cyan',
@@ -12,6 +20,7 @@ const singerStyle = {
 };
 
 function App() {
+  const nayoks = ['Salman Shah', 'Rubel', 'Bappa Raz', 'Joshim', 'Shakib Khan', 'Riyaz', 'Razzak'];
   return (
     <div className="App">
       {/* <div className="music">
@@ -19,9 +28,20 @@ function App() {
         <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
         <p style={{ color: 'black', backgroundColor: 'powderblue' }}>Name: {singer2.name} {singer2.job}</p>
       </div> */}
-      <Person name='Salman Shah' nayika='Sabnur'></Person>
-      <Person name='Rebel' nayika='Moushumi'></Person>
-      <Person name='Bappa Raz' nayika='ChekaKhay'></Person>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* <Person name='Salman Shah' nayika='Sabnur'></Person>
+      <Person name='Rubel' nayika='Moushumi'></Person>
+      <Person name='Bappa Raz' nayika='Cheka Khay'></Person> */}
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      <h3>Here new Component!!</h3>
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
       <h3>Here new component !!!</h3>
       <Friend name='Sobuj' address='Tangail' phone='017********'></Friend>
       <Friend name='Ashik' address='Pabna' phone='018********'></Friend>
@@ -32,8 +52,8 @@ function App() {
 function Person(props) {
   return (
     <div className='person'>
-      <h1>Nayok: {props.name}</h1>
-      <h3>Nayika: {props.nayika}</h3>
+      <h1>Name: {props.name}</h1>
+      <h3> {props.nayika}</h3>
     </div>
   );
 };
